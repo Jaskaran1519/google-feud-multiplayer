108fcccb17b3d2779c869c06bf3c8a86f7dc491d
Jaskaran1519 <jaskaran1519@gmail.com>
2024-11-06 20:47:02 +0530

    frontend added

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,14 @@
+import { Check } from "lucide-react";
 import React from "react";
+
+
+
 
 const page = () => {
   return (
     <div className="w-[90%] mx-auto max-w-[1600px] min-h-screen ">
-      <div className="max-w-[700px] mx-auto my-5 text-center">
-        <h1 className="text-3xl md:text-4xl lg:text-6xl xl:text-7xl font-bold">
+      <div className="max-w-[700px] mx-auto mt-16 mb-10 text-center">
+        <h1 className="text-3xl md:text-4xl lg:text-6xl xl:text-8xl font-bold">
           Googussy
         </h1>
       </div>
@@ -16,11 +20,21 @@ const page = () => {
           JOIN ROOM
         </button>
       </div>
-      <div className="w-fit mx-auto mt-16 flex flex-col space-2">
-        <h1 className=""></h1>
+      <div className="w-fit mx-auto mt-16 flex flex-col gap-5">
+        <div className="flex items-center gap-5 px-2">
+          <h1 className="text-xl md:text-2xl font-semibold">Player's name</h1>
+          <button className="bg-zinc-600 hover:bg-zinc-800 text-white p-1 rounded-full">
+            <Check/>
+          </button>
+        </div>
+        <input
+          type="text"
+          placeholder=""
+          className="rounded-full w-full py-2 px-1 border-black border-[1px]"
+        />
       </div>
     </div>
   );
-};  
+};
 
 export default page;
