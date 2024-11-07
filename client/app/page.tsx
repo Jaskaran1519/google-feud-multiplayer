06cb7904ed9db0f590ffa543d95d9e685123ec44
N8River <aniketdhiman145@gmail.com>
2024-11-07 12:05:29 +0530

    createRoom function was created

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,34 @@
-import { Check } from "lucide-react";
-import React from "react";
+"use client";
 
-const page = () => {
+import { Check } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { BACKEND_URL, JSON_HEADERS } from "../config/config.js";
+
+const Page = () => {
+  const router = useRouter();
+
+  const createRoom = async () => {
+    try {
+      const response = await fetch(`${BACKEND_URL}api/rooms/create`, {
+        method: "POST",
+        headers: JSON_HEADERS,
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to create room");
+      }
+
+      const data = await response.json();
+      const { roomName } = data;
+
+      // Navigate to the room page
+      router.push(`/${roomName}`);
+    } catch (error) {
+      console.error("Error creating room:", error);
+    }
+  };
+
   return (
     <div className="w-[90%] mx-auto max-w-[1600px] min-h-screen ">
       <div className="max-w-[700px] mx-auto mt-16 mb-10 text-center">
@@ -10,16 +37,17 @@ const page = () => {
         </h1>
       </div>
       <div className="mt-32 flex flex-col space-y-5 mx-auto w-fit">
-        <button className="text-2xl font-semibold border-[1px] border-black px-5 py-2 hover:bg-zinc-900 hover:text-white duration-300">
+        <button
+          onClick={createRoom}
+          className="text-2xl font-semibold border-[1px] border-black px-5 py-2 hover:bg-zinc-900 hover:text-white duration-300"
+        >
           CREATE ROOM
         </button>
         <button className="text-2xl font-semibold border-[1px] border-black px-5 py-2 hover:bg-zinc-900 hover:text-white duration-300">
           JOIN ROOM
         </button>
       </div>
-      <div
-        className="w-fit mx-auto mt-16 flex flex-col gap-5"
-      >
+      <div className="w-fit mx-auto mt-16 flex flex-col gap-5">
         <div className="flex items-center gap-5 px-2">
           <h1 className="text-xl md:text-2xl font-semibold">Player's name</h1>
           <button className="bg-zinc-600 hover:bg-zinc-800 text-white p-1 rounded-full">
@@ -36,4 +64,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
